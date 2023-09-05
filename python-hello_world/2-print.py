672e49fc8da2317b2e2be5ba42990aba316616fc
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-05 10:27:54 -0700

    Adding script line for file

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,5 +1,2 @@
-def main():
-    print("Programming is like building a multilingual puzzle")
-
-if __name__ == "__main__":
-    main()
+#!/usr/bin/python3
+print("Programming is like building a multilingual puzzle")
