5e7c86399c9e9cc265a9d4942781ea877ca2afc4
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-07 14:17:54 -0700

    Copying over original file for 3-print_alphabt

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 for char_code in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(char_code), end='')
+    if chr(char_code) not in ['q', 'e']:
+        print("{:c}".format(char_code), end='')
