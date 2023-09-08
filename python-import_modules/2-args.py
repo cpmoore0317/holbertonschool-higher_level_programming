44cc0cf1b47ab79559ff1b20d86d7d6e77f55a21
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-07 20:58:39 -0700

    test for args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,8 +7,8 @@ args = sys.argv[1:]
 num_args = len(args)
 
 if num_args == 0:
-    print("0 arguments.")
+    print("0 argument" + ("" if num_args == 1 else "s") + ".")
 else:
-    print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
+    print(f"{num_args} argument" + ("" if num_args == 1 else "s") + ":")
     for i, arg in enumerate(args, start=1):
         print(f"{i}: {arg}")
