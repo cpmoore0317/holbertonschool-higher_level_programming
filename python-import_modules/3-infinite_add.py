623e90f237837abbabf66591955d603fa53d8688
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-07 21:15:57 -0700

    Testing

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-
-    import sys
-
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+from sys import argv as arg
+__name__ == "__main__":
+total = 0
+for i in range(len(arg) - 1):
+    total += int(arg[i + 1])
+print("{}".format(total))
