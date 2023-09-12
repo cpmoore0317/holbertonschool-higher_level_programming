eb575fea79639396e103d729244f596ca3a79ad9
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-12 10:41:10 -0700

    Testing for division list func

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError("out of range")
+            if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
+                if my_list_2[i] == 0:
+                    raise ZeroDivisionError("division by 0")
+                result.append(my_list_1[i] / my_list_2[i])
+            else:
+                raise TypeError("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        finally:
+            pass  # Continue processing the next element
+    return result
