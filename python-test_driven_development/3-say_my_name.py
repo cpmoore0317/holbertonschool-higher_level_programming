bb1fe7c6096e5d14e7307ddc2ff0c428479e62c8
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-19 14:03:32 -0700

    Adding expectationg statement

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    """
+    Print a message with the first name and last name.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name (default is an empty string).
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+
+    Example:
+        >>> say_my_name("John", "Smith")
+        My name is John Smith
+        >>> say_my_name("Walter", "White")
+        My name is Walter White
+        >>> say_my_name("Bob")
+        My name is Bob
+        >>> say_my_name(12, "White")
+        Traceback (most recent call last):
+            ...
+        TypeError: first_name must be a string or last_name must be a string
+    """
+    if not isinstance(first_name, str) or not isinstance(last_name, str):
+        raise TypeError("first_name must be a string or last_name must be a string")
+
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
+
+if __name__ == '__main__':
+    say_my_name("John", "Smith")
+    say_my_name("Walter", "White")
+    say_my_name("Bob")
+    try:
+        say_my_name(12, "White")
+    except Exception as e:
+        print(e)
