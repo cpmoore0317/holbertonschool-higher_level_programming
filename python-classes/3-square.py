15ee5b0734aa5902bda344cf5a40c96941db0bf9
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-19 10:34:15 -0700

    Moving area func placement

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,9 +15,11 @@ class Square:
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        
+        if size < 0:
             raise ValueError("size must be >= 0")
+        
         self.__size = size
 
-        def area(self):
-            return self.__size ** 2
+    def area(self):
+        return self.__size ** 2
