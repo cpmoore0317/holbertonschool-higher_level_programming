caa8ca4984bf3d47728a1420d46b7a1b2eb9669c
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-19 11:06:16 -0700

    Adding class doclines for 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -2,6 +2,7 @@
 """ Writing a class Square that defines a square. """
 
 class Square:
+    """ Defining a class that represents a square. """
     def __init__(self, size=0):
         self.__size = size
 
