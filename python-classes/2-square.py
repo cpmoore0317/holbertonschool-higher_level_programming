5de3e71438f1cb8d40e491868a2d60a1f385519a
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-19 10:11:53 -0700

    Testing area func

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,8 +4,11 @@ class Sqaure:
     """ Defining class Square """
     def __init__(self, size=0):
         """ Initialization of square """
-        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
