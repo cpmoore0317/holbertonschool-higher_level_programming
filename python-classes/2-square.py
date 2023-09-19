d9a620c961065eae6670ff91f84c0e3a25e292ff
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-19 10:24:12 -0700

    Test putting Name

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+# 2-square.py
+# Parker Moore
 """ Defines a square based on 1-square.py. """
 
 
 class Sqaure:
-    """ Defining class Square. """
+    """ Defining an empty class named Square. """
 
     def __init__(self, size=0):
         """ Initializing Square with a size. """
