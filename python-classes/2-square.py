c05c65818d92a08687acd9189a35e30b16733a62
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-19 10:17:34 -0700

    Testing declaration of size before if else statements

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,11 +3,13 @@
 
 
 class Sqaure:
-    """ Defining class Square """
+    """ Defining class Square. """
 
     def __init__(self, size=0):
+        """ Initializing Square with a size. """
+        self.__size = size
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
