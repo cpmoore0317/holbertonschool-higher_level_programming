ab6c82d6c160d89eaef42597c0922af285e362ba
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-20 09:23:25 -0700

    Fixinf line length

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,7 +8,7 @@ class Square:
 
     __size (int): The size of the square.
 
-    __init__(self, size=0): Initializes a new Square instance with the given size.
+    __init__(self, size=0): Initializes a new Square instance with the size.
     area(self): Calculates and returns the area of the square.
     """
 
