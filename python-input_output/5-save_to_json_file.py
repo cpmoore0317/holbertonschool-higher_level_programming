ce5740aad0c047174d9798d84d5d4e1048372502
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-26 16:00:23 -0700

    Testing for JSON num 5 using with opne

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+A function that writes an Object to a text file,
+using a JSON representation
+"""
+
+def save_to_json_file(my_obj, filename):
+    """
+    Serialize an object to its JSON representation and save it to a text file.
+
+    :param my_obj: The object to be serialized to JSON.
+    :param filename: The name of the file where the JSON data will be saved.
+    """
+    import json
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
