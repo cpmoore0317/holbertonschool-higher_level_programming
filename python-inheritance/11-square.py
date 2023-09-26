0340f263c4cc87070cd3357e6445115c9e0b9ce2
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-26 11:02:12 -0700

    Testing for 11 n7

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
 """
-Class Square that inherits from Rectangle
+This module contains one class: Square, that inherits from Rectangle
 """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """
-    A class representing a square, which is a special case of a rectangle.
-
-    Attributes:
-        __size (int): The size of the square.
-
-    Methods:
-        __init__(self, size): Initializes a Square instance with a given size.
-        area(self): Calculates and returns the area of the square.
-        __str__(self): Returns a string representation of the Square.
+    A class representing a square.
     """
+
     def __init__(self, size):
         """
-        Initializes a Square instance with a given size.
+        Initialize a Square instance.
 
         Args:
             size (int): The size of the square.
@@ -27,23 +21,22 @@ class Square(Rectangle):
             TypeError: If size is not an integer.
             ValueError: If size is less than or equal to 0.
         """
-        self.integer_validator("size", size)
-        self.__size = size
-
-    def area(self):
-        """
-        Calculates and returns the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size ** 2
+        super().__init__(size, size)
 
     def __str__(self):
         """
-        Returns a string representation of the Square.
+        Return a string representation of the Square instance.
 
         Returns:
             str: A string in the format "[Square] <size>/<size>".
         """
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        return "[Square] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """
+        Calculate and return the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__width * self.__height
