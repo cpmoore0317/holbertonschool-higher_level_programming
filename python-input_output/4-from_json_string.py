d91f8856bfd0e4b7770d655637f4130b3884a272
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-26 15:19:12 -0700

    Fixes for 4 JSON str

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+A function that returns an object (Python data structure)
+represented by a JSON string
+"""
+
+
+def from_json_string(my_str):
+    """
+    Deserialize a JSON-formatted string and
+    return the corresponding Python data structure.
+
+    :param my_str: A JSON-formatted string.
+    :return: A Python data structure representing the deserialized object.
+    """
+    import json
+
+    return json.loads(my_str)
