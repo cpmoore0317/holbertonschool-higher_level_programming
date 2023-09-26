236ef62e5f5282b9862cc21f434f8ceb105feff5
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-26 12:46:45 -0700

    Testing for 0 read line num 2

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
+"""
+Read file function
+"""
+
+
 def read_file(filename=""):
+    """
+    Read and print the contents of a text file.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        None
+
+    Notes:
+        This function reads the contents of a text file and prints them to the standard output (stdout).
+        If the file does not exist, it silently handles the `FileNotFoundError` without raising an exception.
+
+    Example:
+        >>> read_file("my_file.txt")
+        This is the content of my_file.txt.
+    """
     try:
         with open(filename, mode='r', encoding='utf-8') as file:
             print(file.read(), end="")
