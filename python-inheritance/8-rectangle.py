ddd4e1eedefaee5a5653ad6e3e0d9d880fd26159
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-26 10:34:46 -0700

    Test for 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,38 +1,16 @@
 #!/usr/bin/python3
 """
-Class for num 8
+This module contains one class: Rectangle, that inherits from BaseGeometry
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
-    """
-    A class representing a rectangle, inheriting from BaseGeometry.
-    """
-
+    """ Contains __init__ method """
     def __init__(self, width, height):
         """
-        Initializes a Rectangle instance with width and height.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-
-        Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is less than or equal to 0.
+        Initialization
         """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width).integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def __str__(self):
-        """
-        Returns a string representation of the Rectangle.
-
-        Args:
-            None
-
-        Returns:
-            str: A string representation of the Rectangle.
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
