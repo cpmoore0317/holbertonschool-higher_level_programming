8f0461de02494d941f34dea4b1e8952873f1e2cc
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-26 10:51:09 -0700

    Test for 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """
-Class for num 10
+This module contains one class: Square, that inherits from Rectangle
 """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """
-    A class representing a square, inheriting from Rectangle.
+    A class representing a square.
     """
 
     def __init__(self, size):
         """
-        Initializes a Square instance with a given size.
+        Initialize a Square instance.
 
         Args:
             size (int): The size of the square.
@@ -20,18 +21,17 @@ class Square(Rectangle):
             TypeError: If size is not an integer.
             ValueError: If size is less than or equal to 0.
         """
-        self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(size, size)
+        super().__init__(size, size)  # Call the parent class's constructor with the same size for both width and height
 
     def __str__(self):
         """
-        Returns a string representation of the Square.
-
-        Args:
-            None
+        Return a string representation of the Square instance.
 
         Returns:
-            str: A string representation of the Square.
+            str: A string in the format "[Square] <size>/<size>".
         """
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)  # Access private attributes of the base class
+
+    # The area() method is inherited from Rectangle, so there's no need to override it here
+
+    # You don't need to define integer_validator here as it's inherited from Rectangle
