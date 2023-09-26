3df310a94d4114ed214e81ba34d5e5c9e3570d22
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-26 12:57:07 -0700

    Testing for 1 write to file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+"""
+A function that writes a string to a text file (UTF8) and returns the
+number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Write text to a text file and return the number of characters written.
+
+    Args:
+        filename (str): The name of the file to be written.
+        text (str): The text to be written to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+
+    Notes:
+        This function opens the specified file in write mode ('w') and writes the provided text to it.
+        If the file already exists, it will be overwritten. If the file does not exist, it will be created.
+        The function returns the number of characters successfully written to the file.
+
+    Example:
+        >>> nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+        >>> print(nb_characters)
+        24
+    """
+    try:
+        with open(filename, mode='w', encoding='utf-8') as file:
+            nb_characters = file.write(text)
+        return nb_characters
+    except Exception:
+        return 0
+
+if __name__ == "__main__":
+    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+    print(nb_characters)
