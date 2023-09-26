165542d2d118ccbe00086555984e3c0cccfdd19c
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-26 10:08:08 -0700

    Testing for 6 n3

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Class for num 6
+"""
+class BaseGeometry:
+    """
+    A base geometry class.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message "area() is not implemented."
+
+        Args:
+            None
+
+        Raises:
+            Exception: Always raises an Exception with the specified message.
+        """
+        raise Exception("area() is not implemented")
