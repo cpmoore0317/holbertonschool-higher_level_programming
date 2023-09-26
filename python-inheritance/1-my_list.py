048ade55f4558ff8d6c9acc531fe9b4a11953eb1
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-26 10:20:45 -0700

    Pycode for 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,6 +7,7 @@ class MyList(list):
     A custom list class that inherits from the built-in list class.
     """
 
+
     def print_sorted(self):
         """
         Prints the list in ascending sorted order.
