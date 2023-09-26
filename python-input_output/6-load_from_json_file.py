a2333e3c27fcb5aac7880f2320c53f6c6eb1cef6
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-26 16:04:52 -0700

    Testing for load from JSON file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+A function that creates an Object from a “JSON file”
+"""
+
+
+def load_from_json_file(filename):
+    """
+    Deserialize a JSON file and return the corresponding Python data structure.
+
+    :param filename: The name of the JSON file to be loaded.
+    :return: A Python data structure representing the deserialized object.
+    """
+    import json
+
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
