4166b7c3f3a550d75e8f3821dbe39cba17658b86
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-26 10:03:29 -0700

    Testing for 2

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Class MyList
+"""
+class MyList(list):
+    """
+    A custom list class that inherits from the built-in list class.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending sorted order.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
