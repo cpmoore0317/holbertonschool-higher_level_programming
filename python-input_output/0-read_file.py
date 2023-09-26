2f398d2c4a51e2781c9ec221258e16c478cd22d4
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-26 12:43:05 -0700

    Testing for 0 read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def read_file(filename=""):
+    try:
+        with open(filename, mode='r', encoding='utf-8') as file:
+            print(file.read(), end="")
+    except FileNotFoundError:
+        pass
+
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
