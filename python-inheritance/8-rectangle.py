f4570ef228070bb4ef40079d11b209c4fefe91d0
“cpmoore0317” <kingpkayy@gmail.com>
2023-09-26 10:43:30 -0700

    Testing for 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,11 +6,32 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Contains __init__ method """
+    """
+    A class representing a rectangle.
+    """
+
     def __init__(self, width, height):
         """
-        Initialization
+        Initialize a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If either width or height is not an integer.
+            ValueError: If either width or height is less than or equal to 0.
         """
-        super().integer_validator("width", width).integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def __str__(self):
+        """
+        Return a string representation of the Rectangle instance.
+
+        Returns:
+            str: A string in the format "[Rectangle] <width>/<height>".
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
