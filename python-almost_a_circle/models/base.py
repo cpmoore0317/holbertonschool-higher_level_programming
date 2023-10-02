05df5a5535dfe0d7745fe1ac8dda15d9621c5096
Parker Moore <cpmoore317@gmail.com>
2023-10-02 16:56:22 -0700

    Setting up for project and number 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module for the Base class.
+"""
+
+class Base:
+    """
+    The `Base` class serves as the foundation for all other classes in
+    this project.
+
+    Attributes:
+        __nb_objects (int): A private class attribute to keep track of the
+        number of instances created.
+
+        id (int): A public instance attribute representing the unique
+        identifier of an instance.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize a new instance of the `Base` class.
+
+        Args:
+            id (int, optional): An optional integer to set as the unique identifier.
+            If not provided, a unique identifier will be automatically generated.
+        """
+        if id in not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
