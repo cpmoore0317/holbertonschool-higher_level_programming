9c4f2d5fd72528069a3241df8c14ee799d503f95
Parker Moore <cpmoore317@gmail.com>
2023-10-02 17:48:01 -0700

    Setting up update method in square class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -57,6 +57,33 @@ class Square(Rectangle):
         self.width = size
         self.height = size
 
+    def update(self, *args, **kwargs):
+        """
+        Update the attributes of the square with the provided arguments.
+
+        Args:
+            *args: Positional arguments to update the square attributes in
+            the following order:
+                1st argument: id attribute
+                2nd argument: size attribute
+                3rd argument: x attribute
+                4th argument: y attribute
+            **kwargs: Keyword arguments to update the square attributes
+            by name.
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     def __str__(self):
         """
         Return a string representation of the square.
