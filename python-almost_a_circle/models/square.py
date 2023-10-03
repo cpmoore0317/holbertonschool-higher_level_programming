b5f03154c58462b58d2ee08c0382ff1e5292873f
Parker Moore <cpmoore317@gmail.com>
2023-10-02 17:42:23 -0700

    Setting up the square class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Module for the Square class.
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    The `Square` class represents a square and inherits from
+    the `Rectangle` class.
+
+    Attributes:
+        id (int): An identifier for the square.
+        size (int): The size of the square.
+        x (int): The x-coordinate of the square's position.
+        y (int): The y-coordinate of the square's position.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialize a new instance of the `Square` class.
+
+        Args:
+            size (int): The size of the square.
+            x (int, optional): The x-coordinate of the square's
+            position (default is 0).
+            y (int, optional): The y-coordinate of the square's
+            position (default is 0).
+            id (int, optional): An optional identifier for the
+            square (default is None).
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Return a string representation of the square.
+
+        Returns:
+            str: A string in the format "[Square] (<id>) <x>/<y> - <size>"
+        """
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
