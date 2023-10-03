07ddc5b8f75edb6959eeb3ca934a1d871f85c95d
Parker Moore <cpmoore317@gmail.com>
2023-10-02 17:29:08 -0700

    Altercations to the display method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -147,8 +147,10 @@ class Rectangle(Base):
         """
         Display the rectangle using # characters.
         """
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
-            print("#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """
