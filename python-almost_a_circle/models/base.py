a2ff7f9f610ccfde1790728809a86bc88da9b2a4
Parker Moore <cpmoore317@gmail.com>
2023-10-02 17:56:25 -0700

    Adding json string method to base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -34,3 +34,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+     @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Convert a list of dictionaries to a JSON string.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+
+        Returns:
+            str: A JSON string representation of the list of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
