185cf68af526b65c54211ea67f4fe251dcb7eb24
Parker Moore <cpmoore317@gmail.com>
2023-10-02 17:58:14 -0700

    Fixing syntax for str to json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -35,7 +35,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-     @staticmethod
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Convert a list of dictionaries to a JSON string.
