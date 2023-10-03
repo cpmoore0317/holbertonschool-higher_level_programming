b807f5bd8ba515e18282fc40c33c69b24e0903ce
Parker Moore <cpmoore317@gmail.com>
2023-10-02 17:34:56 -0700

    Updating the update method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -175,15 +175,20 @@ class Rectangle(Base):
                 3rd argument: height attribute
                 4th argument: x attribute
                 5th argument: y attribute
+                **kwargs: Keyword arguments to update the rectangle
+                attributes by name.
         """
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
-            
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
