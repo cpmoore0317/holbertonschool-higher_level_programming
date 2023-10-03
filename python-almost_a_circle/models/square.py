b6edb8f54dc517f28bef247b9677f132864c6b2f
Parker Moore <cpmoore317@gmail.com>
2023-10-02 17:54:32 -0700

    Adding to_dict on square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -32,6 +32,20 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    def to_dictionary(self):
+        """
+        Get a dictionary representation of the square.
+
+        Returns:
+            dict: A dictionary containing the attributes of the square.
+        """
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
+
     @property
     def size(self):
         """
