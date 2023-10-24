43c6399e13c286edbfba6b7933738184e13ab222
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2023-10-24 15:40:29 -0500

    Tests for task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,24 +8,16 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    # Check if the script is run as the main program.
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    # Connect to the MySQL database.
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
 
-    # Create a cursor to execute SQL queries.
-    cursor = db.cursor()
-
-    # Execute the SQL query to select all states from the 'states' table.
-    cursor.execute("SELECT * FROM states")
-
-    # Fetch all the results.
-    states = cursor.fetchall()
-
-    # Display the results.
-    for state in states:
-        print(state)
-
-    # Close the cursor and the database connection.
-    cursor.close()
-    db.close()
+    for row in rows:
+        print(row)
