a3b92c751e10a4cf9ba0803d88a8388a86435e2f
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2023-10-24 10:18:38 -0500

    Changes for testing on task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- Creates the database hbtn_0d_usa and the table cities.
 CREATE database IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
