415582fa7929c347d59fe98c4e30dec13dfdcee5
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2023-10-24 16:07:14 -0500

    Tests for 0 and 1

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -15,9 +15,12 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    rows = cursor.fetchall()
 
     for row in rows:
         print(row)
+
+    cursor.close()
+    db.close()
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,14 +8,6 @@ from sys import argv
 
 if __name__ == '__main':
     """
-    Import guard
+    Import guard.
     """
-    # Access the database and retrieve the states.
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
-
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
+    
