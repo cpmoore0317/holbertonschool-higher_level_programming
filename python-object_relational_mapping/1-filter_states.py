072db0b5cd80c829c9e3b64ee325abb452da5b7a
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2023-10-24 16:01:34 -0500

    Testing docline addition for task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,6 +7,9 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main':
+    """
+    Import guard
+    """
     # Access the database and retrieve the states.
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
