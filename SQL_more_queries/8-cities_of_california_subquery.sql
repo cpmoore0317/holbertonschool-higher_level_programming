f4eeb664b9eefd256c0ca765e6348f6e8db485f3
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2023-10-24 12:05:10 -0500

    Testing fixes for task 8 California

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- Lists all the cities of California that can be found in the database hbtn_0d_usa.
+SELECT id, name FROM cities WHERE state id = (
+    SELECT id FROM states WHERE name = 'California'
+);
