d8db171b3df45f5b8c85bb535c278cdf2dd2289b
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2023-10-24 10:11:57 -0500

    UNIQUE add for task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- Creates the database hbtn_0d_usa and the table cities.
 CREATE database IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
