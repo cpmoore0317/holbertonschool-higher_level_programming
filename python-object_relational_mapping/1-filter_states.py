2c4291f08ede27bd082ae8186b90f1424c444c74
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2023-10-24 15:57:51 -0500

    Testing changes for task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
 """
-This script lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+This script lists all states with a name starting with 'N' (uppercase 'N') from the database hbtn_0e_0_usa.
 """
 
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
-    
+if __name__ == '__main':
+    # Access the database and retrieve the states.
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
     cursor = db.cursor()
-    cursor.execute("SELECT 8 FROM states \
-                   WHERE name LIKE BINARY 'N%' \
-                 ORDER BY states.id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
     rows = cursor.fetchall()
 
     for row in rows:
