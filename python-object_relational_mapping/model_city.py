756a92748646391c0913c7bd785a6996c66c22d5
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2023-10-24 20:45:31 -0500

    Adding tests for task 14

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """Defines the city class."""
+from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
-from sqlalchemy.orm import relationship
+
 
 class City(Base):
-    """City class that represents the 'cities' table."""
+    """City class
+
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The id of the class
+        name (str): The name of the class
+        state_id (int): The state the city belongs to
+
+    """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    # Define a relationship to the State model
-    state = relationship("State")
