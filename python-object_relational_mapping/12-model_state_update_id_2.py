176b1834080ee0841038ed2ef75bd526cede105b
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2023-10-24 20:46:33 -0500

    Testing fixes for task 12

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python3
 """Change the name of a State object in the database hbtn_0e_6_usa."""
-import sys
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
-if __name__ == "__main":
-    # Check for the correct number of command-line arguments
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
+if __name__ == "__main__":
+    """
+    Updates a State object on the database.
+    """
 
-    # Extract command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Create an engine to connect to the MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(mysql_username, mysql_password, database_name))
-
-    # Create a session to interact with the database
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    # Query and change the name of the State object where id is 2 to "New Mexico"
-    state_to_update = session.query(State).filter(State.id == 2).first()
-
-    if state_to_update:
-        state_to_update.name = "New Mexico"
-        session.commit()
-
-    # Close the session when you're done
+    ari_state = session.query(State).filter(State.id == '2').first()
+    ari_state.name = 'New Mexico'
+    session.commit()
     session.close()
