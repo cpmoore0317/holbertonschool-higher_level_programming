c6182533e6a72334d9792c2b96d4939ac0b3de8b
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2023-10-24 20:54:21 -0500

    Tests for numero 12

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,8 +3,8 @@
 
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
