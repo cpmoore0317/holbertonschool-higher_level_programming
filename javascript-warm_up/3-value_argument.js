35d1aba1437f19dd898539fa4ba1bb6c5ba033f6
cpmoore0317 <kingpkayy@gmail.com>
2023-11-22 14:23:40 -0600

    Using does not equal undefined instead of only if

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (args) {
-    console.log(args[0])
+if (args[0] !== undefined) {
+  console.log(args[0]);
 } else {
-    console.log("No argument")
+  console.log('No argument');
 }
