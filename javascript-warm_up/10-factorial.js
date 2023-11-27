28d5a13ac46a98cd8740fe9d5ccc54e2ff83ea29
cpmoore0317 <kingpkayy@gmail.com>
2023-11-26 21:24:16 -0600

    semistandard for 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 
-function factorial(n) {
-    if (isNaN(n)) {
-      return 1;
-    }
-    if (n === 0 || n === 1) {
-      return 1;
-    }
-    return n * factorial(n - 1);
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
   }
-  
-  const arg = process.argv[2];
-  const result = factorial(parseInt(arg));
-  
-  console.log(result);
-  
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const arg = process.argv[2];
+const result = factorial(parseInt(arg));
+
+console.log(result);
