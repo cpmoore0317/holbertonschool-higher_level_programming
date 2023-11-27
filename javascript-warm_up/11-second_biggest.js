19cb44c741d1ace3a6ee63d53e1fccf220ec27c6
cpmoore0317 <kingpkayy@gmail.com>
2023-11-26 21:15:50 -0600

    Local working with task11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2).map(Number);
+
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  const uniqueNumbers = Array.from(new Set(args)); // Remove duplicates
+  const sortedNumbers = uniqueNumbers.sort((a, b) => b - a); // Sort in descending order
+  console.log(sortedNumbers[1]);
+}
