e27b2ac7f99bf6fd11a225d1989950c399a6bfa4
cpmoore0317 <kingpkayy@gmail.com>
2023-11-26 21:12:42 -0600

    Testing for task7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const x = parseInt(process.argv[2]);
+
+if (Number.isInteger(x) && x > 0) {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
+} else {
+  console.log("Missing number of occurrences");
+}
