fffa8eba4f48e8dfefeb70afe411a97e12551217
cpmoore0317 <kingpkayy@gmail.com>
2023-11-26 21:23:08 -0600

    semistandard for 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,8 +4,8 @@ const x = parseInt(process.argv[2]);
 
 if (Number.isInteger(x) && x > 0) {
   for (let i = 0; i < x; i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
 } else {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 }
