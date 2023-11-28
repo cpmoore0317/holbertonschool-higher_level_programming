7d949db843aa1b760b437bea68b527c797b300cd
cpmoore0317 <kingpkayy@gmail.com>
2023-11-28 13:40:16 -0600

    Locally working print for task3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    // Constructor takes two arguments: w (width) and h (height).
+    // Check if w or h is not a positive integer or is equal to 0.
+    if (w <= 0 || h <= 0) {
+      // If the conditions are met, create an empty object.
+      return {};
+    } else {
+      // Otherwise, initialize the instance attributes width and height with the provided values.
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    // Instance method print() prints the rectangle using the character 'X'.
+    for (let i = 0; i < this.height; i++) {
+      // Loop through each row (height) of the rectangle.
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        // Append 'X' to the row for each column (width) of the rectangle.
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Rectangle;
