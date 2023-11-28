3f5d3d64b4d22609fc6a5da2a601f544bcc52879
cpmoore0317 <kingpkayy@gmail.com>
2023-11-28 13:39:14 -0600

    Semistandard fix for task2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    // Constructor takes two arguments: w (width) and h (height).
+    // Check if w or h is not a positive integer or is equal to 0.
+    if (w <= 0 || h <= 0) {
+      // If the conditions are met, create an empty object.
+      return {};
+    } else {
+      // Otherwise, initialize the instance attributes width and height with the provided values.
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
