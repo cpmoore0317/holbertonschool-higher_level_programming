e36c84839864752b5515e4789d1689470d664b84
cpmoore0317 <kingpkayy@gmail.com>
2023-11-28 13:37:48 -0600

    Working task1

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    // Constructor takes two arguments: w (width) and h (height).
+    // Instance attributes width and height are initialized with the provided values.
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
