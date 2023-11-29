38e1a73f7afdd1cbeea33a893fe0bd3fd089b84a
cpmoore0317 <kingpkayy@gmail.com>
2023-11-28 18:03:45 -0600

    Setting up task4 and testing 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,9 +4,9 @@ class Rectangle {
   constructor (w, h) {
     // Constructor takes two arguments: w (width) and h (height).
     // Check if w or h is not a positive integer or is equal to 0.
-    if (w <= 0 || h <= 0) {
+    if (w <= 0 && h <= 0) {
       // If the conditions are met, create an empty object.
-      return {};
+      return this;
     } else {
       // Otherwise, initialize the instance attributes width and height with the provided values.
       this.width = w;
