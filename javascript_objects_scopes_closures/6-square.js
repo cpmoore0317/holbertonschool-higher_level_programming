e79bf9c6e5d362b75f170536560bc2030a0b81c4
cpmoore0317 <kingpkayy@gmail.com>
2023-11-28 18:21:01 -0600

    charPrint locally working for task6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// Import the Rectangle class from the './4-rectangle' module
+const Rectangle = require('./4-rectangle');
+
+// Define and export a new class Square that extends the Rectangle class
+module.exports = class Square extends Rectangle {
+  // Constructor for the Square class, taking a single argument 'size'
+  constructor (size) {
+    // Call the constructor of the base class (Rectangle) with 'size' for both width and height
+    super(size, size);
+  }
+
+  // Override the double method inherited from the Rectangle class
+  double () {
+    // Call the double method of the base class (Rectangle) using super
+    super.double();
+  }
+
+  charPrint (c = 'X') {
+    super.print(c);
+  }
+};
