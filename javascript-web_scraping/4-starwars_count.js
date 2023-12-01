ee1a34e76ae2ac107a9c9675375566f156215367
cpmoore0317 <kingpkayy@gmail.com>
2023-11-30 20:38:34 -0600

    Testing for star wars count with 10

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,31 @@
 #!/usr/bin/node
 
+// Importing the 'request' module for making HTTP requests
 const request = require('request');
 
-const apiUrl = process.argv[2];
-const characterId = 18;
+// Accessing command line arguments
+const argv = process.argv;
 
-request(apiUrl, (error, response, body) => {
+// Making a request to the provided URL
+request(argv[2], function (error, response, body) {
+  // Handling errors, if any
   if (error) {
     console.error(error);
   } else {
-    const filmsData = JSON.parse(body);
-    const filmsWithWedge = filmsData.results.filter((film) =>
-      film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)
-    );
+    // Parsing the JSON response to extract Star Wars films information
+    const starWarsFilms = JSON.parse(body).results;
 
-    console.log(filmsWithWedge.length);
+    // Counting the number of times character '18' appears in films
+    let numTimes = 0;
+    for (const film of starWarsFilms) {
+      for (const character of film.characters) {
+        if (character.includes('18')) {
+          numTimes += 1;
+        }
+      }
+    }
+
+    // Logging the result
+    console.log(numTimes);
   }
 });
