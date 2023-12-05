407c44c171e6f764ee27edbe1eb0b672e95a633e
cpmoore0317 <kingpkayy@gmail.com>
2023-12-05 13:46:26 -0600

    Working script for task 1

---
+++ javascript-web_jquery/1-script.js
@@ -0,0 +1,7 @@
+// 1-script.js
+
+// Wait for DOM to be fully loaded
+$(document).ready(function () {
+    // Select <header> element and update the color to red
+    $("header").css("color", "#FF0000");
+});
