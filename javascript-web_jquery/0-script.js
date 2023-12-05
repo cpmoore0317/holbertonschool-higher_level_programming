327b301a7c19839d071b599d5288ee20c7f666a2
cpmoore0317 <kingpkayy@gmail.com>
2023-12-05 13:33:15 -0600

    Pusing script for task 0

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,9 @@
+// 0-script.js
+
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    // Select <header> element using querySelector
+    let headerElement = document.querySelector("header");
+    // Update the color of the text
+    headerElement.style.color = "#FF0000";
+});
